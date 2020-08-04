d09cded904d86a664064a90cff834b4ad6a3fb64
Naeem125 <53903331+Naeem125@users.noreply.github.com>
2020-08-04 09:46:52 +0500

    Plagiarism Checker App

---
+++ Plagiarism Checker Application/AOA_INTERFACE/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AOA_INTERFACE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCheckPala_Click(object sender, EventArgs e)
+        {
+            string Text1 = richTextBox1.Text;
+            string Text2 = richTextBox2.Text;
+            lcs(Text1,Text2); //calling function of lcs
+        }
+
+
+        //Function of LCS
+        void lcs(string X, string Y)
+        {
+            int m = X.Length;
+            int n = Y.Length;
+            int[,] L = new int[m + 9999, n + 9999];
+
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    if (i == 0 || j == 0)
+                        L[i, j] = 0;
+                    else if (X[i - 1] == Y[j - 1])
+                        L[i, j] = L[i - 1, j - 1] + 1;
+                    else
+                        L[i, j] = Math.Max(L[i - 1, j], L[i, j - 1]);
+                }
+            }
+
+            int index = L[m, n];
+            int temp = index;
+            char[] lcs = new char[index + 1];
+
+            lcs[index] = '\0'; // Set the terminating character
+
+            int k = m, l = n;
+            while (k > 0 && l > 0)
+            {
+                if (X[k - 1] == Y[l - 1])
+                {
+                    lcs[index - 1] = X[k - 1];
+                    k--;
+                    l--;
+                    index--;
+                }
+
+                else if (L[k - 1, l] > L[k, l - 1])
+                    k--;
+                else
+                    l--;
+            }       
+            int q;
+            for (q = 0; q <= temp; q++)
+            {
+                string s = lcs[q].ToString();
+                {
+                    //Create a file and store matching word in it.
+                    String path = @"C:\Users\HP\OneDrive\Desktop\User_input.txt";
+                    using (StreamWriter sr = File.AppendText(path))
+                    {
+                        sr.WriteLine(s);
+                        sr.Close();
+                    }
+
+                }
+            }
+
+            //Read all matching words from file and print it.
+            string file_data = System.IO.File.ReadAllText(@"C:\Users\HP\OneDrive\Desktop\User_input.txt");
+            MessageBox.Show("THE LENGHT OF LCS IS "+temp+ " \nThe LCS Is \n"+ file_data + "","Plagiarism Checker");
+        }
+
+
+        private void ButtonForFilesClickHere_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 obj = new Form2();
+            obj.Show();
+        }
+
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Plagiarism Checker Application/AOA_INTERFACE/Form2.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AOA_INTERFACE
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonFileCheckPalgiarism_Click(object sender, EventArgs e)
+        {
+            if (radio_btn_Files.Checked == true)  //if file checking 
+            {
+                
+                    string File_1_data = System.IO.File.ReadAllText(TextBoxFilePath1.Text);
+                    string File_2_data = System.IO.File.ReadAllText(TextBoxFilePath2.Text);
+                    lcs1(File_1_data, File_2_data);//Calling function for checking TWO files plagiarism
+
+              
+            }
+            else if (radio_btn_Folders.Checked == true)  //if folder checking
+            {
+
+                string[] array;
+                array = Directory.GetFiles(textBox1.Text);
+               
+                if (array.Length >= 2)  //if folder contains atleast two files
+                {
+
+                    //checking first two files
+                   
+                    string First_file_data = System.IO.File.ReadAllText(array[0]);
+                    string Second_file_data = System.IO.File.ReadAllText(array[1]);
+
+                    //Caling function for checking folders files plagiarism
+                    lcs2(First_file_data, Second_file_data);
+
+                }
+              }
+        }
+
+        //For File Checking
+        void lcs1(String X, String Y)
+
+        {
+            int m = X.Length;
+            int n = Y.Length;
+            int[,] L = new int[m + 1, n + 1];
+
+
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    if (i == 0 || j == 0)
+                        L[i, j] = 0;
+                    else if (X[i - 1] == Y[j - 1])
+                        L[i, j] = L[i - 1, j - 1] + 1;
+                    else
+                        L[i, j] = Math.Max(L[i - 1, j], L[i, j - 1]);
+                }
+            }
+            int index = L[m, n];
+            int temp = index;
+
+
+            char[] lcs = new char[index + 1];
+            lcs[index] = '\0'; // Set the terminating character
+            int k = m, l = n;
+            while (k > 0 && l > 0)
+            {
+                if (X[k - 1] == Y[l - 1])
+                {
+                    lcs[index - 1] = X[k - 1];
+                    k--;
+                    l--;
+                    index--;
+                }
+                else if (L[k - 1, l] > L[k, l - 1])
+                    k--;
+                else
+                    l--;
+            }
+
+            //temp variable contain lenght of lcs
+            for (int q = 0; q <= temp; q++)
+            {
+                string s = lcs[q].ToString();
+                {
+
+                    //Create new file and store matching words in it.
+                    String path = @"C:\Users\HP\OneDrive\Desktop\INTERFACE.txt";
+                    using (StreamWriter sr = File.AppendText(path))
+                    {
+                        sr.WriteLine(s);
+                        sr.Close();
+                        
+                    }
+                }
+            }
+            //Read all matching characters from file in variable file_data and print it by message box. 
+
+            string file_data = System.IO.File.ReadAllText(@"C:\Users\HP\OneDrive\Desktop\INTERFACE.txt");
+            MessageBox.Show("THE LENGHT OF LCS IS " + temp + " \nThe LCS Is \n" + file_data + "", "Plagairism Checker");
+            
+        }
+
+        //For Folder checking
+        void lcs2(string X, string Y)
+        {
+            int m = X.Length;
+            int n = Y.Length;
+            int[,] L = new int[m + 1, n + 1];
+
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    if (i == 0 || j == 0)
+                        L[i, j] = 0;
+                    else if (X[i - 1] == Y[j - 1])
+                        L[i, j] = L[i - 1, j - 1] + 1;
+                    else
+                        L[i, j] = Math.Max(L[i - 1, j], L[i, j - 1]);
+                }
+            }
+
+            int index = L[m, n];
+            int temp = index;
+            char[] lcs = new char[index + 1];
+
+            lcs[index] = '\0'; // Set the terminating character
+
+            int k = m, l = n;
+            while (k > 0 && l > 0)
+            {
+                if (X[k - 1] == Y[l - 1])
+                {
+                    lcs[index - 1] = X[k - 1];
+                    k--;
+                    l--;
+                    index--;
+                }
+
+                else if (L[k - 1, l] > L[k, l - 1])
+                    k--;
+                else
+                    l--;
+            }
+
+
+            int q;
+            for (q = 0; q <= temp; q++)
+            {
+
+                string s = lcs[q].ToString();
+                {
+                    //Create new file and store matching words in it.
+                    String path = @"C:\Users\HP\OneDrive\Desktop\PROJECT_2Folder.txt";
+                    using (StreamWriter sr = File.AppendText(path))
+                    {
+                        sr.WriteLine(s);
+                        sr.Close();
+                      
+                    }
+                }
+            }
+            //Read all matching characters from file in variable fileData and print it by message box. 
+
+            string fileData = System.IO.File.ReadAllText(@"C:\Users\HP\OneDrive\Desktop\PROJECT_2Folder.txt");
+            MessageBox.Show("THE LENGHT OF LCS IS " + temp + " \nThe LCS Is \n" + fileData + "", "Plagairism Checker");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //To browse folder from pc
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dlg.SelectedPath.ToString();
+            }
+        }
+
+        private void radio_btn_Folders_CheckedChanged(object sender, EventArgs e)
+        {
+            bool visible = true;
+            groupBox_FolderChecking.Visible = visible;
+            groupBox1.Visible = !visible;
+        }
+
+        private void radio_btn_Files_CheckedChanged(object sender, EventArgs e)
+        {
+            bool visible = true;
+            groupBox_FolderChecking.Visible = !visible;
+            groupBox1.Visible = visible;
+        }
+
+        private void btn_SelectFile1_Click(object sender, EventArgs e)
+        {
+            //To browse file from pc
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string path = dlg.FileName.ToString();
+                TextBoxFilePath1.Text = path;
+            }
+        }
+
+        private void TextBoxFilePath1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_SelectFile2_Click(object sender, EventArgs e)
+        {
+            //To browse file from pc
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string path = dlg.FileName.ToString();
+                TextBoxFilePath2.Text = path;
+            }
+        }
+
+        private void ButtonBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 obj = new Form1();
+            obj.Show();
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}   
